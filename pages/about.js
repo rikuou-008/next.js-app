ceeb68a39ff7bfcc20cd4207f26134ef1a58d2ce
Eisuke Hashimoto <hashiz0314@icloud.com>
2021-01-11 15:04:57 +0900

    Next.js-app Commit

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+
+import BaseLayout from "@/components/layouts/BaseLayout";
+import BasePage from "@/components/BasePage";
+
+const About = () => {
+  return (
+    <BaseLayout>
+     <BasePage>
+       <h1>I am about Page</h1>
+     </BasePage>
+    </BaseLayout>
+  )
+}
+
+export default About
---
+++ pages/blogs.js
@@ -0,0 +1,14 @@
+import BaseLayout from "@/components/layouts/BaseLayout";
+import BasePage from "@/components/BasePage";
+
+const Blog = () => {
+  return (
+    <BaseLayout>
+     <BasePage>
+       <div>I am Blog Page</div>
+     </BasePage>
+    </BaseLayout>
+  )
+}
+
+export default Blog
